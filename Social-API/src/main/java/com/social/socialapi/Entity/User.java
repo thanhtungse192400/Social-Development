04265d0329b7d7c38ffd748091646812a7f061cb
nguyen <nguyendeptrai23092005@gmail.com>
2025-12-03 04:49:20 +0700

    them entity

---
+++ Social-API/src/main/java/com/social/socialapi/Entity/User.java
@@ -0,0 +1,11 @@
+package com.social.socialapi.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+}
---
+++ Social-API/src/main/java/com/social/socialapi/Repository/VidRepo.java
@@ -0,0 +1,4 @@
+package com.social.socialapi.Repository;
+
+public class VidRepo {
+}
